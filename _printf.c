8a6d3aabe0436ab21e9fd6620d2ed191dd4aef53
TITUS <mpe2418@yahoo.com>
2022-07-12 11:06:35 -0700

    our work

--- _printf.c
+++ _printf.c
@@ -3,7 +3,7 @@
 /**
  * _printf - formatted output conversion and print data.
  * @format: input string.
- * Return: number of chars printed.
+ *  Return: number of chars printed.
  */
 
 int _printf(const char *format, ...)
@@ -21,12 +21,12 @@ for (i = 0; format && format[i]; i++)
 {
 if (format[i] == '%')
 {
-if (format[i + 1] == '\0')
-{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+print_buf(buffer, ibuf), free(buffer), va_end(arguments);
 return (-1);
 }
 else
-{	function = get_print_func(format, i + 1);
+{
+function = get_print_func(format, i + 1);
 if (function == NULL)
 {
 if (format[i + 1] == ' ' && !format[i + 2])
