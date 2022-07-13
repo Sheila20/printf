f137bf740dca709fa3da7bd551eeef7be68d3d49
TITUS <mpe2418@yahoo.com>
2022-07-13 12:58:25 -0700

    correct

--- helpers.c
+++ helpers.c
@@ -3,34 +3,39 @@
 /**
  * print_number - prints out an integer
  * @n: the integer to be printed out
+ *
  * return: Always Successful (0)
  */
 int print_number(int n)int print_number(int n)
 {
-if (n < 0)
-{
-_putchar('-');
-n = -n;
-}
-n = -n;
-print_number(n / 10);
-_putchar(n % 10 + '0');
-return (0);
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+		print_number(n / 10);
+	_putchar(n % 10 + '0');
+
+	return (0);
 }
 
 /**
  * print_string - prints out a string
  * @c: the string to be printed out
+ *
  * Return: Always Successful (0)
  */
+
 int print_string(char *c)
 {
-int len = strlen(c);
-int i = 0;
-while (i < len)
-{
-_putchar(c[i]);
-i++;
-}
-return (0);
+	int len = strlen(c);
+	int i = 0;
+
+	while (i < len)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	return (0);
 }
